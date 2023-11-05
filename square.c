fae7261fb10a7dc6dde0a630b42b9fee85e75e40
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2023-11-05 19:47:45 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,50 @@
+#include <GL/glut.h>
+#include <stdio.h>
+
+static GLfloat spin = 0.0;
+
+void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+    glRotatef(spin, 0.0, 0.0, 1.0);
+    glColor3f(1.0, 1.0, 1.0);
+    glRectf(-25.0, -25.0, 25.0, 25.0);
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+void spinDisplay(void) {
+    spin = spin + 2.0;
+    if (spin > 360.0) {
+        spin = spin - 360.0;
+    }
+    glutPostRedisplay();
+}
+
+void init(void) {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
+}
+
+void reshape(int w, int h) {
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-50.0, 50.0, -50.0, 50.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(spinDisplay);
+    glutMainLoop();
+    return 0;
+}
